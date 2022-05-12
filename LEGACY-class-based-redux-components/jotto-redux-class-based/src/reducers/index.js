af1b98d694e344d54948b9e8a0628be7cc9d4a98
emman216 <emman.c.pagcaliwagan@accenture.com>
2022-05-12 20:03:03 +0800

    Set up guessWords tests

--- LEGACY-class-based-redux-components/jotto-redux-class-based/src/reducers/index.js
+++ LEGACY-class-based-redux-components/jotto-redux-class-based/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import success from "./successReducer";
+import guessedWord from "./guessedWordsReducer";
 
 export default combineReducers({
   success,
+  guessedWord,
 });
