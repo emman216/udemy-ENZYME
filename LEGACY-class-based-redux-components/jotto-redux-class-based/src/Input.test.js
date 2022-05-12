2144eb086fb5b3bd91dea06ae30310c4d6e02c1d
emman216 <emman.c.pagcaliwagan@accenture.com>
2022-05-12 19:23:13 +0800

    Complete input component render tests for success=false

--- LEGACY-class-based-redux-components/jotto-redux-class-based/src/Input.test.js
+++ LEGACY-class-based-redux-components/jotto-redux-class-based/src/Input.test.js
@@ -20,15 +20,33 @@ const setup = (initialState = {}) => {
 
 describe("render", () => {
   describe("word has not been guessed", () => {
-    test("renders component without error", () => {});
-    test("renders input box", () => {});
-    test("renders submit button", () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: false };
+      wrapper = setup(initialState);
+    });
+    test("renders component without error", () => {
+      const component = findByTestAttr(wrapper, "component-input");
+      expect(component.length).toBe(1);
+    });
+    test("renders input box", () => {
+      const inputBox = findByTestAttr(wrapper, "input-box");
+      expect(inputBox.length).toBe(1);
+    });
+    test("renders submit button", () => {
+      const submitButton = findByTestAttr(wrapper, "submit-button");
+      expect(submitButton.length).toBe(1);
+    });
   });
-  describe("word has been guessed", () => {});
+  describe("word has been successfully guessed", () => {
+    test("renders component without error", () => {});
+    test("does not render input box", () => {});
+    test("does not render submit button", () => {});
+  });
 });
 
-describe("update state", () => {
-  test("renders component without erre", () => {});
-  test("does not render input box", () => {});
-  test("does not render submit button", () => {});
-});
+// describe("update state", () => {
+//   test("renders component without error", () => {});
+//   test("does not render input box", () => {});
+//   test("does not render submit button", () => {});
+// });
