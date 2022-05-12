473e26822c7f1929fe3a9d6b2b0397c2884ac858
emman216 <emman.c.pagcaliwagan@accenture.com>
2022-05-12 19:05:51 +0800

    Se up Input component and storeFactory test util

--- LEGACY-class-based-redux-components/jotto-redux-class-based/test/testUtils.js
+++ LEGACY-class-based-redux-components/jotto-redux-class-based/test/testUtils.js
@@ -1,18 +1,17 @@
 import checkPropTypes from "check-prop-types";
-import { createStore, applyMiddleware } from "redux";
+import { createStore } from "redux";
+
+import rootReducer from "../src/reducers";
 
 /**
  * Create a testing store with imported reducers, middleware, and initial state.
- *  globals: rootReducer, middlewares.
- * @param {object} initialState - Initial state for store.
+ *  globals: rootReducer
+ * @param {object} initialState - Initial state for store
  * @function storeFactory
  * @returns {Store} - Redux store.
  */
 export const storeFactory = (initialState) => {
-  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
-    createStore
-  );
-  return createStoreWithMiddleware(rootReducer, initialState);
+  return createStore(rootReducer, initialState);
 };
 
 /**
